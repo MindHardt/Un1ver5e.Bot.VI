03e87b3cc84d3c559b1bb06877a36ec9076ab27b
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-07-03 00:30:37 +0500

    Added common way to create deletable messages

---
+++ Un1ver5e.Bot/Commands/DeleteThisButtonCommandModule.cs
@@ -0,0 +1,29 @@
+﻿using Disqord;
+using Disqord.Bot.Commands.Components;
+using Disqord.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Un1ver5e.Bot.Commands
+{
+    public class DeleteThisButtonCommandModule : DiscordComponentModuleBase
+    {
+        public static LocalButtonComponent GetDeleteButton() => new()
+        {
+            Emoji = LocalEmoji.Unicode("🗑"),
+            CustomId = "mo_delete_this",
+            Style = LocalButtonComponentStyle.Danger
+        };
+
+        [ButtonCommand("mo_delete_this")]
+        public async ValueTask DeleteMessageButtonCommand()
+        {
+            IMessage msg = (Context.Interaction as IComponentInteraction)!.Message;
+
+            await Bot.DeleteMessageAsync(msg.ChannelId, msg.Id);
+        }
+    }
+}
